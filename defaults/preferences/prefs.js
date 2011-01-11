89f4a6e492fbf20ef2c7e77c8ed2361d165d32ac
Blake Winton <bwinton@latte.ca>
2011-01-11 11:34:18 -0500

    Initial commit.

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.resizeheader.boolpref", false);
+pref("extensions.resizeheader.intpref", 0);
+pref("extensions.resizeheader.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.resizeheader@latte.ca.description", "chrome://resizeheader/locale/overlay.properties");
