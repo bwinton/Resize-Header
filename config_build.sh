f1130ed229b1695ebbe65fd9fa9af0dab7bf2b1d
Blake Winton <bwinton@latte.ca>
2011-08-17 20:06:03 -0400

    build files.

---
+++ config_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Build config for build.sh
+APP_NAME=resize-headers
+CHROME_PROVIDERS="chrome/content chrome/locale chrome/skin"
+CLEAN_UP=1
+ROOT_FILES=
+ROOT_DIRS="defaults"
+BEFORE_BUILD=
+AFTER_BUILD=
